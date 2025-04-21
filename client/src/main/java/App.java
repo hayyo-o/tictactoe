90df112965a8ff3ed4b8c50e2b640af2b31f23f6
Pavel Liapin <liap00@vse.cz>
2025-04-21 15:13:06 +0200

    Client part dev started

---
+++ client/src/main/java/App.java
@@ -0,0 +1,24 @@
+package cz.tictactoe.client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Hlavní třída klienta pro spuštění JavaFX GUI.
+ */
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/layout.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Piškvorky - Klient");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
